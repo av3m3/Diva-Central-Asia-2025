43a40674c124691f494b9325aebda591b0e2ec86
av3m3 <102965284+av3m3@users.noreply.github.com>
2025-06-01 00:31:50 +0500

    Update main.py

--- main.py
+++ main.py
@@ -39,3 +39,6 @@ async def process_update(request: Request):
 def main():
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
